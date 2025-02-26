79f6913a0a3d55308dabf1593d78de224e73e845
Jeffrey Oyuela <jeffreysmac@Jeffreys-MacBook-Pro.local>
2025-02-26 14:55:24 -0500

    Added React Router and pages folder

---
+++ front-end/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import yippeeCat from '../assets/yippee.gif';
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>This is the Home Page!!</h1>
+            <img src={yippeeCat} alt="Cat Jumping and Clapping"/>
+        </div>
+    );
+}
