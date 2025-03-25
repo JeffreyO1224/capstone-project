43a219c6cce4cf5ca2e1518cfb10da557ba926a3
JeffreyO1224 <jeffreyoyuela@gmail.com>
2025-03-25 19:55:34 -0400

    Updating server.js for reference comment

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -16,6 +16,16 @@ app.post('/hello', function(req, res) {
     res.send('Hello, ' + req.body.name + ' from Post!');
 });
 
+//For reference
+/*
+app.post('/api/articles/:name/like', function(req, res) {
+    const article = articleInfo.find(a => a.name === req.params.name);
+    article.upvotes += 1;
+
+    res.send('Success! The article ' + req.params.name + ' now has ' + article.likes + ' likes!');
+});
+*/
+
 app.listen(8000, function() {
     console.log('Server is listening on port 8000');
 });
