76b17a8c99d8f4dcdf6a78c32763bea717d8548a
JeffreyO1224 <jeffreyoyuela@gmail.com>
2025-04-05 18:47:31 -0400

    Added location support

--- front-end/src/pages/MapPage/MapPage.tsx
+++ front-end/src/pages/MapPage/MapPage.tsx
@@ -1,10 +1,64 @@
 import "leaflet/dist/leaflet.css";
 import './MapPage.css';
 import petData from './PetData.js';
+import { useState, useEffect } from 'react';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMapEvent, useMap } from 'react-leaflet';
+import { LatLng, Icon } from "leaflet";
+
+//Clicking on the map automatically sets the view to wherever you clicked
+function SetViewOnClick() {
+    const map = useMapEvent('click', (e) => {
+      map.setView(e.latlng, map.getZoom(), {
+        animate: true,
+      })
+    })
+  
+    return null
+}
+
+function CenterOnCurrentLocation() {
+    const map = useMap();
+
+    map.locate();
+
+    useMapEvents({
+        locationfound(e) {
+            map.flyTo(e.latlng, map.getZoom());
+        }
+    })
+
+    return null;
+}
+
+const currentLocationMarkerIcon = new Icon({
+    iconUrl: '../../assets/current-location-marker.png',
+    iconSize: [38, 38] // size of the icon
+});
+
+const petLocationMarkerIcon = new Icon({
+    iconUrl: "../../assets/pet-marker-icon.png",
+    iconSize: [38, 38],
+})
+
+function LocationMarker() {
+    const [position, setPosition] = useState<LatLng | null>(null);
+
+    const map = useMapEvents({
+        locationfound(e) {
+            setPosition(e.latlng)
+        },
+    });
+
+    return position === null ? null : 
+        <Marker position={position}>
+            <Popup>Your Location</Popup>
+        </Marker>
+    ;
+}
 
 export default function MapPage() {
+    
     const petMarkers = petData.map((marker) => {
         return (
             <Marker position={marker.lastSeen}>
@@ -19,11 +73,14 @@ export default function MapPage() {
     })
 
     return (
-        <MapContainer center={[40.7678, -73.9645]} zoom={50} scrollWheelZoom={true}>
+        <MapContainer center={{lat: 40.7678, lng: -73.9645}} zoom={50} scrollWheelZoom={true} >
+            <SetViewOnClick />
+            <CenterOnCurrentLocation />
             <TileLayer 
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <LocationMarker />
             {petMarkers}
         </MapContainer>
     );
