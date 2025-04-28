8faea85fee300e6b550e28c0b76b4970bdf722fb
Tamar abelson <tamarabelson@Tamars-MacBook-Pro.local>
2025-04-28 10:54:38 -0400

    updated sql + sign up

--- lost_pets.sql
+++ lost_pets.sql
@@ -3,10 +3,11 @@
  * or users trying to help finding missing pets)
  */
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    email VARCHAR(255) NOT null UNIQUE
+	user_name VARCHAR(50) PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(255) NOT NULL UNIQUE,
+	password VARCHAR(50) NOT NULL
 );
 
 /* LOST_PET table where users can post their missing pet. user_id is used as foreign key 
@@ -15,11 +16,12 @@ CREATE TABLE users (
  */
 CREATE TABLE lost_pet (
     post_id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    dog_name VARCHAR(100) NOT NULL,
+    user_name VARCHAR NOT NULL,
+    pet_name VARCHAR(100) NOT NULL,
     location VARCHAR(255) NOT NULL,
     status VARCHAR(10) CHECK(status IN ('not found', 'found')) DEFAULT 'not found',
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    image_url TEXT,
+    FOREIGN KEY (user_name) REFERENCES users(user_name) ON DELETE CASCADE
 );
 
 /* PET_SIGHT table is where users who saw a potential match to missing pet posts can post 
@@ -31,25 +33,12 @@ CREATE TABLE pet_sight (
     sight_id SERIAL PRIMARY KEY,
     post_id INTEGER NOT NULL,
     location VARCHAR(255) NOT NULL,
+    image_url TEXT,
     FOREIGN KEY (post_id) REFERENCES lost_pet(post_id) ON DELETE CASCADE
 );
 
-INSERT INTO users (first_name, last_name, email)
-VALUES ('TAMAR', 'ABELSON', 'tamar@test.com'), 
-	   ('MICHAEL', 'SIMON', 'simon@test.com');
 
-SELECT * FROM users;
-SELECT * FROM users;
 
-DROP TABLE users CASCADE;
-
-CREATE TABLE users (
-	user_name VARCHAR(50) PRIMARY KEY,
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
-	email VARCHAR(255) NOT NULL UNIQUE,
-	password VARCHAR(50) NOT NULL
-);
 
 
 
