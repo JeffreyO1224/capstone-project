318a5d1aff315263a9f64c099ab2cdb8a57712ab
gustavogrijalba <34608825+gustavogrijalba@users.noreply.github.com>
2025-05-08 17:29:54 -0400

    added a more robust db connection w/ loggign and also integrated aiven

--- back-end/src/database.js
+++ back-end/src/database.js
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 import { Pool } from 'pg';
 
+//load our environment variables
 config();
 
 console.log("Loaded DB config:", {
@@ -16,7 +17,57 @@ const database = new Pool({
     user: process.env.DB_USER,
     port: Number(process.env.DB_PORT),
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
+//use our sql scheme to create the tables in our aiven database
+const schema = `
+CREATE TABLE IF NOT EXISTS users (
+    user_name VARCHAR(50) PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS lost_pet (
+    post_id SERIAL PRIMARY KEY,
+    user_name VARCHAR NOT NULL,
+    pet_name VARCHAR(100) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    status VARCHAR(10) CHECK(status IN ('not found', 'found')) DEFAULT 'not found',
+    image_url TEXT,
+    FOREIGN KEY (user_name) REFERENCES users(user_name) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS pet_sight (
+    sight_id SERIAL PRIMARY KEY,
+    post_id INTEGER NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    image_url TEXT,
+    FOREIGN KEY (post_id) REFERENCES lost_pet(post_id) ON DELETE CASCADE
+);
+`;
+
+//connect to the data explicitly and also ensure that we are connected to the database
+//with logging to the console
+//using a cloud db solution that is free
+database.connect((err, client, release) => {
+    if (err) {
+        return console.error('❌ Connection error', err.stack);
+    }
+
+    client.query(schema, (err) => {
+        release(); // release the client
+        if (err) {
+            return console.error('❌ Error running schema', err.stack);
+        }
+        //ensure our aiven database creates the stables
+        console.log('✅ Schema successfully created and DB connected.');
+    });
+})
 
 export default database;
